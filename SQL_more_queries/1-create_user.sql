330a6a8e47c331db7ada50ef954cc07667f0e63a
Khadija <khadijabeja@gmail.com>
2024-01-11 00:07:59 +0300

    Root user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Active: 1704829387245@@127.0.0.1@3306@khadija_b
+--Root user
+CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
