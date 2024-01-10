48b78e151bd6964c17ec8b34d6f1cf7f9f4d58a6
Khadija <khadijabeja@gmail.com>
2024-01-11 00:00:14 +0300

    GRANT PRIVILEGES

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,5 @@
 -- Active: 1704829387245@@127.0.0.1@3306@khadija_b
 -- GIVING PRIVILEGES TO A USER
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
