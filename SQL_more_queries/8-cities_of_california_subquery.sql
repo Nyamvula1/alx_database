0f23ae9cc46bb517f25842c1b0f184136d9ef638
Khadija <khadijabeja@gmail.com>
2024-01-11 23:55:01 +0300

    ORDER

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Active: 1704829387245@@127.0.0.1@3306@khadija_b
 -- selecting fields from the table
-SELECT cities.id, cities.name FROM cities, states WHERE cities.state_id = state.id AND states.name = 'California'
+SELECT cities.id, cities.name FROM cities, states WHERE cities.state_id = states.id AND states.name = 'California'
 ORDER BY cities.id ASC;
