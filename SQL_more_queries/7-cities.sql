f69a96f5012cce54c6e156ff29f5225e41e152fe
Khadija <khadijabeja@gmail.com>
2024-01-11 20:30:31 +0300

    cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,10 @@
 -- Active: 1704829387245@@127.0.0.1@3306@khadija_b
+-- CREATING A FOREIGN KEY
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'cities'(
     `id`       INT          NOT NULL AUTO_INCREMENT,
     `state_id` INT          NOT NULL,
     `name`     VARCHAR(256) NOT NULL,
     FOREIGN KEY(`state_id`)
-    REFERENCES `hbtn_0d_usa`.`states`(`id`)
+   
 );
